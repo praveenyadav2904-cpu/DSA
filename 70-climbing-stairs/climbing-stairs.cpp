461813efa275f516c3cecae15ea0fe676be034f7
Praveen Yadav <praveenyadav2904@gmail.com>
2026-05-28 20:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.95%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1)
+        return 1;
+        if(n==2)
+        return 2;
+        int a=1;
+        int b=2;
+        int next=0;
+        for(int i=3;i<=n;i++){
+         next=a+b;
+         a=b;
+         b=next;
+        }
+        return b;
+    }
+};
