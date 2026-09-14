d73b78d0569126413968e32ed47882cb0de10def
Praveen Yadav <Praveenyadav2904@gmail.com>
2026-09-14 19:25:04 +0530

    Basic OOPS

---
+++ hello.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class student{
+    public:
+    void print(){
+        cout<<"Hello how do you do?"<<endl;
+    }
+    void animal(){
+        cout<<"cow does not bark"<<endl;
+    }
+    void color(){
+        cout<<"red is the color of the apple"<<endl;
+    }
+};
+int main()
+{
+student pk;
+pk.print();
+pk.animal();
+pk.color();
+cout<<"Prince is sleeping"<<endl;
+cout<<"Prince is eating"<<endl;
+cout<<"Prince is playing"<<endl;
+cout<<"praveen is a good boy"<<endl;
+cout<<"prince is a good boy"<<endl;
+cout<<"It is good now"<<endl;
+return 0;
+}
+
