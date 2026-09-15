e0d06d4ec9b33c451b6f8eae2a8d29ded4e9f16b
Praveen Yadav <praveenyadav2904@gmail.com>
2026-09-15 22:59:06 +0530

    Find middle eqaul sum index

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n=nums.size();
+        int totalsum=0,leftsum=0;
+        for(int i=0;i<n;i++){
+            totalsum+=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            if(leftsum==totalsum-leftsum-nums[i])
+            return i;
+
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
